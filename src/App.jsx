961032369525777e61828b434bf9e8a4604fb5e5
samuelEvans000 <chintuvincent@gmail.com>
2023-12-08 20:16:57 +0530

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { UserProvider } from "./components/UserContext";
 import Login from "./components/login/login";
 import Dashboard from "./components/dashboard/dashboard";
